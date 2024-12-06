c4a229917a1eb4ece77bd9d0f9f0d446f2ce4f49
ammrbhlwn <150604655+ammrbhlwn@users.noreply.github.com>
2024-12-06 10:51:14 +0700

    Delete src directory

--- src/scripts/globals/api-endpoint.js
+++
@@ -1,9 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  PRODUCT_LIST: `${CONFIG.BASE_URL}/list`,
-  PRODUCT_DETAIL: (id) => `${CONFIG.BASE_URL}/detail/${id}`,
-  ADD_REVIEW: `${CONFIG.BASE_URL}/review`,
-};
-
-export default API_ENDPOINT;
